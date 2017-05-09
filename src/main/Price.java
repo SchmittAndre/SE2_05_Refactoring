abdf33156e1b1d6269e59f30f8fed717df9b804d
SchmittAndre <andre1502@gmx.de>
2017-05-09 11:02:49 +0200

    merged test projekt

---
+++ src/main/Price.java
@@ -0,0 +1,9 @@
+package main;
+
+public abstract class Price {
+	abstract int getPriceCode();
+	abstract double getCharge(int daysRented);
+	 int getFrequentRenterPoints(int daysRented) {
+			 return 1;
+	}
+}
---
+++ src/test/CustomerTest.java
@@ -0,0 +1,13 @@
+package test;
+import main.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CustomerTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
