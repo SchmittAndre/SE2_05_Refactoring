d0d6cd1ac96f65d59941612f56be74631dc3c293
SchmittAndre <andre1502@gmx.de>
2017-05-09 11:16:44 +0200

    rebuild with compiler 1.8

--- src/main/Price.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public abstract class Price {
-	abstract int getPriceCode();
-	abstract double getCharge(int daysRented);
-	 int getFrequentRenterPoints(int daysRented) {
-			 return 1;
-	}
-}
--- src/test/CustomerTest.java
+++ src/test/CustomerTest.java
@@ -1,6 +1,7 @@
 package test;
 import main2.*; 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class CustomerTest {
@@ -9,5 +10,11 @@ public class CustomerTest {
 	public void test() {
 		assertTrue(true);
 	}
-
+	@Test
+	public void testKMovie() {
+		Movie m1 = new Movie("movie1" , 1);
+		assertTrue(m1.getTitle() == "movie1");
+		assertTrue(m1.getPriceCode() == 1); 
+	}
+	
 }
